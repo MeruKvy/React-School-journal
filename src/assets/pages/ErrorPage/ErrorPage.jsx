b814a2ea8a67e1736196aa4a2df27c1f77a5a2ab
giorgi merkviladze <giorgimerkviladze@macbooks-MacBook-Pro-2.local>
2023-12-04 19:22:06 +0400

    github pages

--- src/assets/pages/ErrorPage/ErrorPage.jsx
+++ src/assets/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./errorPage.scss";
-
 const ErrorPage = () => {
   return (
     <div className="error-spacer">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/App.js"
+    "./src/App.js",
+    "./src/assets/components/Grades/test.scss",
+    "./src/assets/components/Modal/modal.scss",
+    "./src/assets/components/Person/person.scss",
+    "./src/assets/pages/ErrorPage/errorPage.scss",
+    "./src/assets/pages/Main/mainPage.scss",
+    "./src/assets/pages/StudentProfile/studentProfile.scss",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
